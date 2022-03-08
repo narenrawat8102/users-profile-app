4288c59fc68243e487cb2139b3a3e96fe6e3274f
narenrawat8102 <rawat.narendra87@gmail.com>
2022-03-08 09:13:10 +0530

    pagination added

--- js/index.js
+++ js/index.js
@@ -2,6 +2,18 @@
 
 let allUsersData = null;
 const baseApiURL = "https://dummyapi.io/data/v1";
+const loaderHTML = `<div class="lds-roller">
+<div></div>
+<div></div>
+<div></div>
+<div></div>
+<div></div>
+<div></div>
+<div></div>
+<div></div>
+</div>`;
+const getContentContainerEle = document.querySelector(".content_container");
+getContentContainerEle.innerHTML = loaderHTML;
 
 // Get All User Promise / Code Producing
 const profilesData = fetch(`${baseApiURL}/user?page=1&limit=10`, {
@@ -20,66 +32,19 @@ profilesData
     allUsersData = data.data;
 
     createUserTemplate(allUsersData);
+    document.querySelector(".pagination_container").classList.remove("hide");
   })
   .then((data) => {
-    const getAllViewDetailsBtns = document.querySelectorAll(".view-details");
-    for (let i = 0; i < getAllViewDetailsBtns.length; i++) {
-      const viewDetailsBtn = getAllViewDetailsBtns[i];
-
-      viewDetailsBtn.addEventListener("click", function () {
-        const getModalBodyEle = document.querySelector(".modal-body");
-        getModalBodyEle.innerHTML = `<img class="loader" src="./assets/loader.gif" alt="" />`;
-        const appId = viewDetailsBtn.getAttribute("data-id");
-
-        // Get Single User Promise / Code Producing
-        const profileData = fetch(`${baseApiURL}/user/${appId}`, {
-          method: "GET",
-          headers: {
-            "Content-Type": "application/json",
-            "app-id": "62244a20de40fc669036d85b",
-          },
-        });
-
-        profileData
-          .then((res) => res.json())
-          .then((user) => {
-            const userDetailsTemplate = `
-            <div class="pic">
-               <img src="${user.picture}" alt=""/>
-            </div>
-            <div class="contact">
-               <h3>Contact Details:</h3>
-               <br />
-               <p><b>Name: </b>${
-                 user.title + " " + user.firstName + " " + user.lastName
-               }</p>
-               <p><b>Gender: </b>${user.gender}</p>
-               <p><b>Date of Birth: </b>${user.dateOfBirth}</p>
-               <p><b>Register Date: </b>${user.registerDate}</p>
-               <p><b>Email: </b>${user.email}</p>
-               <p><b>Phone: </b>${user.phone}</p>
-            </div>
-            <div class="address">
-               <h3>Address:</h3>
-               <br />
-               <p><b>State: </b>${user.location.state}</p>
-               <p><b>Street: </b>${user.location.street}</p>
-               <p><b>City: </b>${user.location.city}</p>
-               <p><b>Country: </b>${user.location.country}</p>
-               <p><b>Timezone: </b>${user.location.timezone}</p>
-            </div>`;
-
-            getModalBodyEle.innerHTML = userDetailsTemplate;
-          });
-      });
-    }
+    addClickHandlerOnViewDetails();
   })
   .then((data) => {
     const getAllPageBtns = document.querySelectorAll(".page_num");
     for (let i = 0; i < getAllPageBtns.length; i++) {
       const pageBtn = getAllPageBtns[i];
 
-      pageBtn.addEventListener("click", function () {
+      pageBtn.addEventListener("click", function (e) {
+        getContentContainerEle.innerHTML = loaderHTML;
+        e.preventDefault();
         for (let i = 0; i < getAllPageBtns.length; i++) {
           const singleBtn = getAllPageBtns[i];
 
@@ -105,6 +70,9 @@ profilesData
             allUsersData = data.data;
             createUserTemplate(allUsersData);
             console.log(data);
+          })
+          .then((data) => {
+            addClickHandlerOnViewDetails();
           });
       });
     }
@@ -130,4 +98,58 @@ function createUserTemplate(usersList) {
 
   const getContentContainerEle = document.querySelector(".content_container");
   getContentContainerEle.innerHTML = allUsersStr;
+}
+
+function addClickHandlerOnViewDetails() {
+  const getAllViewDetailsBtns = document.querySelectorAll(".view-details");
+  for (let i = 0; i < getAllViewDetailsBtns.length; i++) {
+    const viewDetailsBtn = getAllViewDetailsBtns[i];
+
+    viewDetailsBtn.addEventListener("click", function () {
+      const getModalBodyEle = document.querySelector(".modal-body");
+      getModalBodyEle.innerHTML = `<img class="loader" src="./assets/loader.gif" alt="" />`;
+      const appId = viewDetailsBtn.getAttribute("data-id");
+
+      // Get Single User Promise / Code Producing
+      const profileData = fetch(`${baseApiURL}/user/${appId}`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          "app-id": "62244a20de40fc669036d85b",
+        },
+      });
+
+      profileData
+        .then((res) => res.json())
+        .then((user) => {
+          const userDetailsTemplate = `
+            <div class="pic">
+               <img src="${user.picture}" alt=""/>
+            </div>
+            <div class="contact">
+               <h3>Contact Details:</h3>
+               <br />
+               <p><b>Name: </b>${
+                 user.title + " " + user.firstName + " " + user.lastName
+               }</p>
+               <p><b>Gender: </b>${user.gender}</p>
+               <p><b>Date of Birth: </b>${user.dateOfBirth}</p>
+               <p><b>Register Date: </b>${user.registerDate}</p>
+               <p><b>Email: </b>${user.email}</p>
+               <p><b>Phone: </b>${user.phone}</p>
+            </div>
+            <div class="address">
+               <h3>Address:</h3>
+               <br />
+               <p><b>State: </b>${user.location.state}</p>
+               <p><b>Street: </b>${user.location.street}</p>
+               <p><b>City: </b>${user.location.city}</p>
+               <p><b>Country: </b>${user.location.country}</p>
+               <p><b>Timezone: </b>${user.location.timezone}</p>
+            </div>`;
+
+          getModalBodyEle.innerHTML = userDetailsTemplate;
+        });
+    });
+  }
 }
